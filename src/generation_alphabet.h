656daed5f4584c8323d5354e829fed1173c431b3
ennse1 <Lagg81@mail.ru>
2021-10-18 00:15:00 +0700

    Add reaction for empty alphabet

--- src/generation_alphabet.h
+++ src/generation_alphabet.h
@@ -9,6 +9,6 @@ int generation_alphabet(
         string* alphabet,
         int* length_alphabet,
         bool digit_flag,
-        bool small_character_flag,
-        bool large_character_flag,
+        bool small_symbol_flag,
+        bool large_symbol_flag,
         bool special_symbol_flag);
